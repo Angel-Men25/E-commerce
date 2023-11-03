4421576537a537cf14face0ab00eff7dbf91fa9f
Angel-Men25 <angel.valdez.25@hotmail.com>
2023-11-03 13:31:50 -0600

    Añade opciones en el select dinamicamente

--- js/credit-card.js
+++ js/credit-card.js
@@ -2,11 +2,31 @@ const tarjeta = document.querySelector('#tarjeta');
 const btnAbrirFormulario = document.querySelector('#btn-abrir-formulario');
 const form = document.querySelector('#form-tarjeta');
 
+// rotacion de la tarjeta
 tarjeta.addEventListener('click', () => {
   tarjeta.classList.toggle('active');
 })
 
+// boton de abrir formulario
 btnAbrirFormulario.addEventListener('click', () => {
   btnAbrirFormulario.classList.toggle('active');
   form.classList.toggle('active');
 })
+
+// select del mes generado dinamicamente
+for (let i = 1; i <= 12; i++) {
+  let option = document.createElement('option');
+  option.value = i;
+  option.innerText = i;
+  form.selectMes.appendChild(option);
+}
+
+// select del año generado dinamicamente
+const actualYear = new Date().getFullYear();
+const maxYear = actualYear + 8;
+for (let i = actualYear; i <= maxYear; i++) {
+  let option = document.createElement('option');
+  option.value = i;
+  option.innerText = i;
+  form.selectYear.appendChild(option);
+}
